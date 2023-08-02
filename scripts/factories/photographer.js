e3667ab4333cb79ad1b61b2dbbf05215c1e8729f
Amel Benni <amel.benni@gmail.com>
2023-08-02 17:23:04 +0200

    add url param to href

--- scripts/factories/photographer.js
+++ scripts/factories/photographer.js
@@ -7,8 +7,13 @@ function photographerFactory(data) {
         const article = document.createElement( 'article' );
         const img = document.createElement( 'img' );
         img.setAttribute("src", picture)
+        img.setAttribute("alt", name)
         const h2 = document.createElement( 'h2' );
         h2.textContent = name;
+        const link = document.createElement( 'a' );
+        link.setAttribute('href', 'photographer.html/?id=' + id)
+        link.appendChild(img);
+        link.appendChild(h2);
         const h3 = document.createElement( 'h3');
         h3.textContent = city + ", " + country;
         const p = document.createElement( "p" );
